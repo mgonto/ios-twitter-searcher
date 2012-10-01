94e6212021245bafd08bebf6fe70095a9b594b7e
Pablo Grigolatto <grigo@mini-conos-mac.internal.zauberlabs.com>
2012-10-01 14:10:42 -0300

    Initial Commit

---
+++ twitter-searcher/TGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TGAppDelegate.h
+//  twitter-searcher
+//
+//  Created by Pablo Grigolatto on 10/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter-searcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twitter-searcher
+//
+//  Created by Pablo Grigolatto on 10/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TGAppDelegate class]));
+    }
+}
